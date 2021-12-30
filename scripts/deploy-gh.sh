c50f7d491c0eecaa4b7065c83976c7c57d3f2d5b
jiahaixin <Jstarfish3.14>
2021-12-30 21:12:47 +0800

    :books:blog deploy

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -7,10 +7,10 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd ../docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-#echo 'www.starfish.ink/JavaKeeper' > CNAME
+#echo 'http://javakeeper.starfish.ink/' > CNAME
 
 git init
 git add -A
@@ -25,5 +25,6 @@ git commit -m 'deploy'
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
 
 git push -f git@github.com:jstarfish/jstarfish.github.io.git master
+
 
 cd -
